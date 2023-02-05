299142291d1b0c64bebbab2f8dd148a27a2693a1
nakir323 <nakirdev@gmail.com>
2023-02-06 00:33:06 +0900

    Add settings for using MUI & tailwind together

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Specify body instead of root because MUI dialog is placed outside of root
+  important: '#body',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+  },
+  corePlugins: {
+    // Remove Tailwind CSS's preflight style so it can use the MUI's preflight instead (CssBaseline).
+    preflight: false,
   },
   plugins: [],
 }
